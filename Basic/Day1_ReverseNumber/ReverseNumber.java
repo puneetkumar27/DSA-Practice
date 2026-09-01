e24ef49874c0188eec2acd1619300ad38a126bdc
Puneet Kumar <harshitshukla67287@gmail.com>
2026-09-01 14:03:48 +0530

    ReverseNumber

--- Basic/Day1_ReverseNumber/ReverseNumber.java
+++ Basic/Day1_ReverseNumber/ReverseNumber.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class ReverseNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number to reverse: ");
-
         int num = sc.nextInt();
-        int reverse = 0;
 
+        int reverse = 0;
         while (num != 0) {
             int digit = num % 10;
             reverse = reverse * 10 + digit;
