5c7349c5046a63122abfa9ccd21c06eeb904648e
Puneet Kumar <harshitshukla67287@gmail.com>
2026-09-13 21:34:41 +0530

    Add Factorial program

---
+++ Basic/Day2_Basics/Factorial.java
@@ -0,0 +1,25 @@
+// Write a Java program to find the factorial of a given number.
+package Basic.Day2_Basics;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial = " + factorial);
+
+        sc.close();
+    }
+}
