2439df3add5f147f013e358c0ed5f0db905ba7e7
Puneet Kumar <harshitshukla67287@gmail.com>
2026-09-13 22:27:36 +0530

    Add GCD program

---
+++ Basic/Day2_Basics/GCD.java
@@ -0,0 +1,30 @@
+// Write a Java program to find the GCD of two numbers.
+
+package Basic.Day2_Basics;
+
+import java.util.Scanner;
+
+public class GCD {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        while (b != 0) {
+
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD = " + a);
+
+        sc.close();
+    }
+}
